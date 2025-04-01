7125ba99550fd703f532b3056d8bcf7760549381
Debugger12116 <grinevicius.gustas@gmail.com>
2025-04-01 15:14:14 +0300

    -

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 const countries = [
-    { country: "Albania", capital: "Tirana" },
-    { country: "Andorra", capital: "Andorra la Vella" },
-    { country: "Austria", capital: "Vienna" },
-    { country: "Belarus", capital: "Minsk" },
-    { country: "Belgium", capital: "Brussels" },
-    { country: "Bosnia and Herzegovina", capital: "Sarajevo" },
-    { country: "Bulgaria", capital: "Sofia" },
-    { country: "Croatia", capital: "Zagreb" },
-    { country: "Cyprus", capital: "Nicosia" },
-    { country: "Czech Republic", capital: "Prague" },
-    { country: "Denmark", capital: "Copenhagen" },
-    { country: "Estonia", capital: "Tallinn" },
-    { country: "Finland", capital: "Helsinki" },
-    { country: "France", capital: "Paris" },
-    { country: "Germany", capital: "Berlin" },
-    { country: "Greece", capital: "Athens" },
-    { country: "Hungary", capital: "Budapest" },
-    { country: "Iceland", capital: "Reykjavik" },
-    { country: "Ireland", capital: "Dublin" },
-    { country: "Italy", capital: "Rome" },
-    { country: "Latvia", capital: "Riga" },
-    { country: "Liechtenstein", capital: "Vaduz" },
-    { country: "Lithuania", capital: "Vilnius" },
-    { country: "Luxembourg", capital: "Luxembourg" },
-    { country: "Malta", capital: "Valletta" },
-    { country: "Moldova", capital: "Chisinau" },
-    { country: "Monaco", capital: "Monaco" },
-    { country: "Montenegro", capital: "Podgorica" },
-    { country: "Netherlands", capital: "Amsterdam" },
-    { country: "North Macedonia", capital: "Skopje" },
-    { country: "Norway", capital: "Oslo" },
-    { country: "Poland", capital: "Warsaw" },
-    { country: "Portugal", capital: "Lisbon" },
-    { country: "Romania", capital: "Bucharest" },
-    { country: "San Marino", capital: "San Marino" },
-    { country: "Serbia", capital: "Belgrade" },
-    { country: "Slovakia", capital: "Bratislava" },
-    { country: "Slovenia", capital: "Ljubljana" },
-    { country: "Spain", capital: "Madrid" },
-    { country: "Sweden", capital: "Stockholm" },
-    { country: "Switzerland", capital: "Bern" },
-    { country: "Ukraine", capital: "Kyiv" },
-    { country: "United Kingdom", capital: "London" },
-    { country: "Vatican City", capital: "Vatican City" }
+    {country: "Albanija", capital: "Tirana" },
+    { country: "Andora", capital: "Andora" },
+    { country: "Austrija", capital: "Viena" },
+    { country: "Belgija", capital: "Briuselis" },
+    { country: "Bosnia ir Hercegovina", capital: "Sarajevas" },
+    { country: "Bulgarija", capital: "Sofija" },
+    { country: "Kroatija", capital: "Zagrebas" },
+    { country: "Kipras", capital: "Nikosija" },
+    { country: "Čekija", capital: "Praha" },
+    { country: "Danija", capital: "Kopenhaga" },
+    { country: "Estija", capital: "Talinas" },
+    { country: "Suomija", capital: "Helsinkis" },
+    { country: "Prancūzija", capital: "Paryžius" },
+    { country: "Vokietija", capital: "Berlynas" },
+    { country: "Graikija", capital: "Atėnai" },
+    { country: "Vengrija", capital: "Budapeštas" },
+    { country: "Islandija", capital: "Reikjavikas" },
+    { country: "Airija", capital: "Dublinas" },
+    { country: "Italija", capital: "Roma" },
+    { country: "Latvija", capital: "Ryga" },
+    { country: "Lichtenšteinas", capital: "Vaducas" },
+    { country: "Lietuva", capital: "Vilnius" },
+    { country: "Liuksemburgas", capital: "Liuksemburgas" },
+    { country: "Malta", capital: "Valeta" },
+    { country: "Moldova", capital: "Kišiniovas" },
+    { country: "Monakas", capital: "Monakas" },
+    { country: "Juodkalnija", capital: "Podgorica" },
+    { country: "Nyderlandai", capital: "Amsterdamas" },
+    { country: "Šiaurės Makedonija", capital: "Skopjė" },
+    { country: "Norvegija", capital: "Oslas" },
+    { country: "Lenkija", capital: "Varšuva" },
+    { country: "Portugalija", capital: "Lisabona" },
+    { country: "Rumunija", capital: "Bukareštas" },
+    { country: "San Marinas", capital: "San Marinas" },
+    { country: "Serbija", capital: "Belgradas" },
+    { country: "Slovakija", capital: "Bratislava" },
+    { country: "Slovėnija", capital: "Liubliana" },
+    { country: "Ispanija", capital: "Madridas" },
+    { country: "Švedija", capital: "Stokholmas" },
+    { country: "Šveicarija", capital: "Bernas" },
+    { country: "Ukraina", capital: "Kijevas" },
+    { country: "Didižioji Britanija", capital: "Londonas" },
+    { country: "Vatikanas", capital: "Vatikanas" },
+    { country: "Andora", capital: "Andora" }
 ];
 const result = document.getElementById('rezultatas')
 let i = 0
@@ -54,7 +54,7 @@ let salis;
 const mygtukas = document.getElementById('saliuPasirinkimoMygtukas')
 mygtukas.onclick = mygtukopaspaudimas
 
-
+let isCheckingAnswer = false;
 
 
 function mygtukopaspaudimas() {
@@ -64,30 +64,38 @@ function mygtukopaspaudimas() {
     naujasKlausimas()
 }
 
-document.getElementById("atsMygtukas").addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
+document.getElementById("answer").addEventListener("keypress", function(event) {
+    if (event.key === "Enter" && !isCheckingAnswer) {
         checkAnswer();
     }
-}); 
+});
 
 document.getElementById("atsMygtukas").onclick = checkAnswer
 
 function checkAnswer() {
+    isCheckingAnswer = true;
+    document.getElementById("atsMygtukas").disabled = "disabled";
     let atsakymas = document.getElementById('answer').value.trim()
 
     if(atsakymas.toLowerCase() === salis.capital.toLowerCase()) {
         score ++
         result.innerText = 'Teisingai'
         result.style.color = 'green'
+        document.getElementById('rez').innerText = `${score}/${i}`
         setTimeout(naujasKlausimas, 1000)
     } else {
         result.innerText = `Neteisingai. Teisingas atsakymas yra ${salis.capital}`
         result.style.color = 'red'
+        document.getElementById('rez').innerText = `${score}/${i}`
         setTimeout(naujasKlausimas, 1000)
     }
 }
 
 function naujasKlausimas() {
+    document.getElementById('rez').innerText = `${score}/${i}`
+    isCheckingAnswer = false;
+    document.getElementById("atsMygtukas").disabled = "";
+    document.getElementById('answer').value = ''
     result.innerText = ''
 
     if (i < skaicius) {
@@ -102,25 +110,15 @@ function naujasKlausimas() {
 
 
 function gameOver() {
-    /*let pavadinimas2 = document.getElementsByClassName('pavadinimas')
-    let inputas2 = document.getElementById('saliuSkaiciausPasirinkimas')
-    let saliuskaiciuklausimas2 = document.getElementById('saliuskaiciuklausimas')
-    let saliuPasirinkimoMygtukas2 = document.getElementById('saliuPasirinkimoMygtukas')
 
-    pavadinimas2.innerText = ''
-    inputas2.style.display = 'hidden'
-    saliuskaiciuklausimas2.innerText = 'Žaidimas baigėsi'
-    saliuPasirinkimoMygtukas2.style.display = 'hidden'
-    */
     document.getElementById('pagrindinis').hidden = true
     document.getElementById('pabaiga').hidden = false
 
-    if (score === skaicius) {
+    if (score >= skaicius) {
         document.getElementById('pabaigosZinute').innerText = `Pabaiga. Rezultatas: ${score}/${skaicius}`
-        document.getElementById('pabaigosZinute').style.color = 'red'
+        document.getElementById('pabaigosZinute').style.color = 'green'
     } else {
         document.getElementById('pabaigosZinute').innerText = `Pabaiga. Rezultatas: ${score}/${skaicius}`
-        document.getElementById('pabaigosZinute').style.color = 'red'
     }
     
     
